4eb1d4dc30745a0c138f0ffe5c32fc85940cffdc
ALepper <i16013@hb.dhbw-stuttgart.de>
2019-05-13 16:24:59 +0200

    fixed tests

--- KontaktsplitterTests/Parser/SalutationParserTests.cs
+++ KontaktsplitterTests/Parser/SalutationParserTests.cs
@@ -10,121 +10,121 @@ namespace Kontaktsplitter.Parser.Tests
         [TestMethod]
         public void SimpleGermanNameTest()
         {
-            var kunde = new SalutationParser().Parse("Frau");
-            Assert.Equals(kunde.Anrede, "Frau Sandra Berger");
-            Assert.Equals(kunde.Geschlecht, Geschlecht.Weiblich);
-            Assert.Equals(kunde.Nachname, "Berger");
-            Assert.Equals(kunde.Vorname, "Sandra");
-            Assert.Equals(kunde.Titel, null);
-            Assert.Equals(kunde.Briefanrede, "Sehr geehrte Frau");
+            var kunde = new SalutationParser().Parse("Frau  Sandra Berger");
+            Assert.AreEqual(kunde.Anrede, "Frau");
+            Assert.AreEqual(kunde.Geschlecht, Geschlecht.Weiblich);
+            Assert.AreEqual(kunde.Nachname, "Berger");
+            Assert.AreEqual(kunde.Vorname, "Sandra");
+            Assert.AreEqual(kunde.Titel, null);
+            Assert.AreEqual(kunde.Briefanrede, "Sehr geehrte Frau");
         }
 
         [TestMethod]
         public void SimpleTitleGermanNameTest()
         {
             var kunde = new SalutationParser().Parse("Herr Dr. Sandro Gutmensch");
-            Assert.Equals(kunde.Anrede, "Herr Dr. Sandro Gutmensch");
-            Assert.Equals(kunde.Geschlecht, Geschlecht.Männlich);
-            Assert.Equals(kunde.Nachname, "Gutmensch");
-            Assert.Equals(kunde.Vorname, "Sandro");
-            Assert.Equals(kunde.Titel, "Dr");
-            Assert.Equals(kunde.Briefanrede, "Sehr geehrter Herr Dr. Sandro Gutmensch");
+            Assert.AreEqual(kunde.Anrede, "Herrn Dr");
+            Assert.AreEqual(kunde.Geschlecht, Geschlecht.Männlich);
+            Assert.AreEqual(kunde.Nachname, "Gutmensch");
+            Assert.AreEqual(kunde.Vorname, "Sandro");
+            Assert.AreEqual(kunde.Titel, "Dr");
+            Assert.AreEqual(kunde.Briefanrede, "Sehr geehrter Herr Dr");
         }
 
         [TestMethod]
         public void LongLastNameGermanTest()
         {
             var kunde = new SalutationParser().Parse("Professor Heinrich Freiherr vom Wald");
-            Assert.Equals(kunde.Anrede, "Herr Professor Heinrich Freiherr vom Wald");
-            Assert.Equals(kunde.Geschlecht, Geschlecht.Männlich);
-            Assert.Equals(kunde.Nachname, "Freiherr vom Wald");
-            Assert.Equals(kunde.Vorname, "Heinrich");
-            Assert.Equals(kunde.Titel, "Professor");
-            Assert.Equals(kunde.Briefanrede, "Sehr geehrter Herr Professor Heinrich Freiherr vom Wald");
+            Assert.AreEqual(kunde.Anrede, "Herrn Professor");
+            Assert.AreEqual(kunde.Geschlecht, Geschlecht.Männlich);
+            Assert.AreEqual(kunde.Nachname, "Freiherr vom Wald");
+            Assert.AreEqual(kunde.Vorname, "Heinrich");
+            Assert.AreEqual(kunde.Titel, "Professor");
+            Assert.AreEqual(kunde.Briefanrede, "Sehr geehrter Herr Professor");
         }
 
         [TestMethod]
         public void SimpleEnglishNameTest()
         {
             var kunde = new SalutationParser().Parse("Mrs. Doreen Faber");
-            Assert.Equals(kunde.Anrede, "Mrs. Doreen Faber");
-            Assert.Equals(kunde.Geschlecht, Geschlecht.Weiblich);
-            Assert.Equals(kunde.Nachname, "Faber");
-            Assert.Equals(kunde.Vorname, "Doreen");
-            Assert.Equals(kunde.Titel, "");
-            Assert.Equals(kunde.Briefanrede, "Dear Mrs. Doreen Faber");
+            Assert.AreEqual(kunde.Anrede, "Mrs");
+            Assert.AreEqual(kunde.Geschlecht, Geschlecht.Weiblich);
+            Assert.AreEqual(kunde.Nachname, "Faber");
+            Assert.AreEqual(kunde.Vorname, "Doreen");
+            Assert.AreEqual(kunde.Titel, "");
+            Assert.AreEqual(kunde.Briefanrede, "Dear Mrs.");
         }
 
         [TestMethod]
         public void FrenchSimpleNameTest()
         {
             var kunde = new SalutationParser().Parse("Mme. Charlotte Noir");
-            Assert.Equals(kunde.Anrede, "Mme. Charlotte Noir");
-            Assert.Equals(kunde.Geschlecht, Geschlecht.Weiblich);
-            Assert.Equals(kunde.Nachname, "Noir");
-            Assert.Equals(kunde.Vorname, "Charlotte");
-            Assert.Equals(kunde.Titel, "");
-            Assert.Equals(kunde.Briefanrede, "Madame Charlotte Noir");
+            Assert.AreEqual(kunde.Anrede, "Mme");
+            Assert.AreEqual(kunde.Geschlecht, Geschlecht.Weiblich);
+            Assert.AreEqual(kunde.Nachname, "Noir");
+            Assert.AreEqual(kunde.Vorname, "Charlotte");
+            Assert.AreEqual(kunde.Titel, "");
+            Assert.AreEqual(kunde.Briefanrede, "Madame");
         }
 
         [TestMethod]
         public void ItalianLastNameTest()
         {
             var kunde = new SalutationParser().Parse("Estobar y Gonzales");
-            Assert.Equals(kunde.Anrede, "Señor Estobar y Gonzales");
-            Assert.Equals(kunde.Geschlecht, Geschlecht.Männlich);
-            Assert.Equals(kunde.Nachname, "Estobar y Gonzales");
-            Assert.Equals(kunde.Vorname, "");
-            Assert.Equals(kunde.Titel, "");
-            Assert.Equals(kunde.Briefanrede, "Egregio Signor Estobar y Gonzales");
+            Assert.AreEqual(kunde.Anrede, "Señor");
+            Assert.AreEqual(kunde.Geschlecht, Geschlecht.Männlich);
+            Assert.AreEqual(kunde.Nachname, "Estobar y Gonzales");
+            Assert.AreEqual(kunde.Vorname, "");
+            Assert.AreEqual(kunde.Titel, "");
+            Assert.AreEqual(kunde.Briefanrede, "Egregio Signor");
         }
 
         [TestMethod]
         public void LongGermanNameTest()
         {
             var kunde = new SalutationParser().Parse("Frau Prof. Dr. rer. nat. Maria von Leuthäuser-Schnarrenberger");
-            Assert.Equals(kunde.Anrede, "Frau Prof. Dr. rer. nat. Maria von Leuthäuser-Schnarrenberger");
-            Assert.Equals(kunde.Geschlecht, Geschlecht.Weiblich);
-            Assert.Equals(kunde.Nachname, "von Leuthäuser-Schnarrenberger");
-            Assert.Equals(kunde.Vorname, "Maria");
-            Assert.Equals(kunde.Titel, "Prof. Dr. rer. nat.");
-            Assert.Equals(kunde.Briefanrede, "Sehr geehrte Frau Prof. Dr. rer. nat. Maria von Leuthäuser-Schnarrenberger");
+            Assert.AreEqual(kunde.Anrede, "Frau Prof Dr rer nat");
+            Assert.AreEqual(kunde.Geschlecht, Geschlecht.Weiblich);
+            Assert.AreEqual(kunde.Nachname, "von Leuthäuser-Schnarrenberger");
+            Assert.AreEqual(kunde.Vorname, "Maria");
+            Assert.AreEqual(kunde.Titel, "Prof Dr rer nat");
+            Assert.AreEqual(kunde.Briefanrede, "Sehr geehrte Frau Prof Dr rer nat");
         }
 
         [TestMethod]
         public void PrefixTitleGermanNameTest()
         {
             var kunde = new SalutationParser().Parse("Herr Dipl. Ing. Max von Müller");
-            Assert.Equals(kunde.Anrede, "Herr Dipl. Ing. Max von Müller");
-            Assert.Equals(kunde.Geschlecht, Geschlecht.Männlich);
-            Assert.Equals(kunde.Nachname, "von Müller");
-            Assert.Equals(kunde.Vorname, "Max");
-            Assert.Equals(kunde.Titel, "Dipl. Ing.");
-            Assert.Equals(kunde.Briefanrede, "Sehr geehrter Herr Dipl. Ing. Max von Müller");
+            Assert.AreEqual(kunde.Anrede, "Herrn Dipl Ing");
+            Assert.AreEqual(kunde.Geschlecht, Geschlecht.Männlich);
+            Assert.AreEqual(kunde.Nachname, "von Müller");
+            Assert.AreEqual(kunde.Vorname, "Max");
+            Assert.AreEqual(kunde.Titel, "Dipl Ing");
+            Assert.AreEqual(kunde.Briefanrede, "Sehr geehrter Herr Dipl Ing");
         }
 
         [TestMethod]
         public void NamesBackwardsGermanNameTest()
         {
             var kunde = new SalutationParser().Parse("Dr. Russwurm, Winfried");
-            Assert.Equals(kunde.Anrede, "Herr Dr. Winfried Russwurm");
-            Assert.Equals(kunde.Geschlecht, Geschlecht.Männlich);
-            Assert.Equals(kunde.Nachname, "Russwurm");
-            Assert.Equals(kunde.Vorname, "Winfried");
-            Assert.Equals(kunde.Titel, "Dr.");
-            Assert.Equals(kunde.Briefanrede, "Sehr geehrter Herr Dr. Winfried Russwurm");
+            Assert.AreEqual(kunde.Anrede, "Herrn Dr");
+            Assert.AreEqual(kunde.Geschlecht, Geschlecht.Männlich);
+            Assert.AreEqual(kunde.Nachname, "Russwurm");
+            Assert.AreEqual(kunde.Vorname, "Winfried");
+            Assert.AreEqual(kunde.Titel, "Dr.");
+            Assert.AreEqual(kunde.Briefanrede, "Sehr geehrter Herr Dr");
         }
 
         [TestMethod]
         public void MutlipleTitlesGermanNameTest()
         {
             var kunde = new SalutationParser().Parse("Herr Dr.-Ing. Dr. rer.nat. Dr. h.c. mult. Paul Steffens");
-            Assert.Equals(kunde.Anrede, "Herr Dr.-Ing. Dr. rer.nat. Dr. h.c. mult. Paul Steffens");
-            Assert.Equals(kunde.Geschlecht, Geschlecht.Männlich);
-            Assert.Equals(kunde.Nachname, "Steffens");
-            Assert.Equals(kunde.Vorname, "Paul");
-            Assert.Equals(kunde.Titel, "Dr.-Ing. Dr. rer.nat. Dr. h.c. mult.");
-            Assert.Equals(kunde.Briefanrede, "Sehr geehrter Herr Dr.-Ing. Dr. rer.nat. Dr. h.c. mult. Paul Steffens");
+            Assert.AreEqual(kunde.Anrede, "Herrn Dr-Ing Dr rer nat Dr hc mult");
+            Assert.AreEqual(kunde.Geschlecht, Geschlecht.Männlich);
+            Assert.AreEqual(kunde.Nachname, "Steffens");
+            Assert.AreEqual(kunde.Vorname, "Paul");
+            Assert.AreEqual(kunde.Titel, "Dr.-Ing. Dr. rer.nat. Dr. h.c. mult.");
+            Assert.AreEqual(kunde.Briefanrede, "Sehr geehrter Herr Dr-Ing Dr rer nat Dr hc mult");
         }
     }
 }
